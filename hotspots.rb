06efa6ed2671420356e0bf9b4f4eb7e1cd6727ae
Matt Kaniaris <mkaniaris@gmail.com>
2012-06-21 15:28:50 -0700

    Now geting, deleting, adding hooks!

--- hotspots.rb
+++ hotspots.rb
@@ -3,13 +3,24 @@ require 'json'
 require 'sinatra'
 require 'github_api'
 
-def install_hooks
+def handle_hooks
   settings = JSON.parse(File.read 'settings.json')
   puts  login: settings['login'], password: settings['password']
   github = Github.new basic_auth: "#{settings['login']}:#{settings['password']}"
   hooks = github.repos.hooks.all 'racker', 'reach'
-  hooks.delete_if {|hook| hook.name != 'web'}
+  hooks_to_delete = []
+  hooks.each do |hook|
+    if hook.name == "web" and hook.config.url == settings['address'] then
+      hooks_to_delete.push(hook)
+    end
+  end
+  hooks_to_delete.each do |hook|
+    puts 'deletig hook ', hook
+    github.repos.hooks.delete 'racker', 'reach', hook.id
+  end
+  github.repos.hooks.create 'racker', 'reach', name: "web", active: true, config:
+    {url: settings['address'], content_type: "json"}
 end
 
-configure do  install_hooks
+configure do handle_hooks
 end
