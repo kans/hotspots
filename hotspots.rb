9fbd710203736f77741f7ca39dacdf31ae490eff
Matt Kaniaris <mkaniaris@gmail.com>
2012-06-21 15:33:46 -0700

    Using variables instead of hash/hard coding names now

--- hotspots.rb
+++ hotspots.rb
@@ -5,21 +5,24 @@ require 'github_api'
 
 def handle_hooks
   settings = JSON.parse(File.read 'settings.json')
-  puts  login: settings['login'], password: settings['password']
+  org = settings['org']
+  repo = settings['repo']
+  address = settings['address']
+
   github = Github.new basic_auth: "#{settings['login']}:#{settings['password']}"
-  hooks = github.repos.hooks.all 'racker', 'reach'
+  hooks = github.repos.hooks.all org, repo
   hooks_to_delete = []
   hooks.each do |hook|
-    if hook.name == "web" and hook.config.url == settings['address'] then
+    if hook.name == "web" and hook.config.url == address then
       hooks_to_delete.push(hook)
     end
   end
   hooks_to_delete.each do |hook|
     puts 'deletig hook ', hook
-    github.repos.hooks.delete 'racker', 'reach', hook.id
+    github.repos.hooks.delete org, repo, hook.id
   end
-  github.repos.hooks.create 'racker', 'reach', name: "web", active: true, config:
-    {url: settings['address'], content_type: "json"}
+  github.repos.hooks.create org, repo, name: "web", active: true, config:
+    {url: address, content_type: "json"}
 end
 
 configure do handle_hooks
