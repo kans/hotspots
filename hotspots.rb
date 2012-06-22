a5e618d09fe353812d86c81567301c6d8d10aee3
Matt Kaniaris <mkaniaris@gmail.com>
2012-06-22 12:57:42 -0700

    trying to check out the repos on disk

--- hotspots.rb
+++ hotspots.rb
@@ -2,6 +2,8 @@ require 'json'
 
 require 'sinatra'
 require 'github_api'
+require 'debugger'
+require 'grit'
 
 configure do
   settings = JSON.parse(File.read 'settings.json')
@@ -9,33 +11,59 @@ configure do
   repo = settings['repo']
   address = settings['address']
 
+  hook_url = "#{address}/api/#{org}/#{repo}/#{settings['name']}"
+
   github = Github.new basic_auth: "#{settings['login']}:#{settings['password']}"
   hooks = github.repos.hooks.all org, repo
   hooks_to_delete = []
   hooks.each do |hook|
-    if hook.name == "web" and hook.config.url == address then
+    if hook.name == "web" and hook.config.url == hook_url then
       hooks_to_delete.push(hook)
     end
   end
   hooks_to_delete.each do |hook|
-    puts 'deletig hook ', hook
     github.repos.hooks.delete org, repo, hook.id
   end
   github.repos.hooks.create org, repo, name: "web", active: true, config:
-    {url: "#{address}/api/#{org}/#{repo}/#{settings['name']}", content_type: "json"}
-
-  res = github.repos.commits.all org, repo, per_page: 100, page:1
+    {url: hook_url, content_type: "json"}
 
   regex ||= /fix(es|ed)?|close(s|d)?/i
 
-  to_fetch = []
-  res.each_page do |page|
-    page.each do |commit|
-      if commit.commit.message =~ regex
-        to_fetch.append(commit)
-      end
+  Fix = Struct.new(:message, :date, :files)
+  Spot = Struct.new(:file, :score)
+
+  grit = Grit::Repo.new "repos/#{repo}"
+  grit.clone({:branch => 'origin/master'},"git://github.com/#{org}/#{repo}.git", "repos/#{repo}")
+
+  fixes = []
+
+  regex ||= /fix(es|ed)?|close(s|d)?/i
+
+  tree = grit.tree('master')
+
+  commit_list = grit.git.rev_list({:max_count => false, :no_merges => true, :pretty => "raw", :timeout => false}, branch)
+  Grit::Commit.list_from_string(grit, commit_list).each do |commit|
+    if commit.message =~ regex
+      files = commit.stats.files.map {|s| s.first}.select{ |s| tree/s }
+      fixes << Fix.new(commit.short_message, commit.date, files)
     end
   end
+
+  hotspots = Hash.new(0)
+  fixes.each do |fix|
+    fix.files.each do |file|
+      t = 1 - ((Time.now - fix.date).to_f / (Time.now - fixes.last.date))
+      hotspots[file] += 1/(1+Math.exp((-12*t)+12))
+    end
+  end
+
+  spots = hotspots.sort_by {|k,v| v}.reverse.collect do |spot|
+    Spot.new(spot.first, sprintf('%.4f', spot.last))
+  end
+
+
+
+  paths = {}
 
   post "/api/:org/:repo/:name" do
     puts params
