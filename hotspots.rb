fa3292ef21935ecdc0a0f8215354f08c555672cc
Geoff Greer <geoff@greer.fm>
2012-06-22 17:07:04 -0700

    Progress. Put code into functions

--- hotspots.rb
+++ hotspots.rb
@@ -7,87 +7,36 @@ require 'debugger'
 require 'grit'
 require 'haml'
 
-include FileUtils
+require './repo'
+
+$settings = {}
+repos = {}
 
 configure do
-  settings = JSON.parse(File.read 'settings.json')
-  org = settings['org']
-  repo = settings['repo']
-  address = settings['address']
-  login = settings['login']
-  password = CGI::escape settings['password']
+  $settings = JSON.parse(File.read 'settings.json')
+  settings_repos = $settings['repos']
 
-  all_hotspots = {}
-
-  hook_url = "#{address}/api/#{org}/#{repo}/#{settings['name']}"
-
-  # github = Github.new basic_auth: "#{settings['login']}:#{settings['password']}"
-  # hooks = github.repos.hooks.all org, repo
-  # hooks_to_delete = []
-  # hooks.each do |hook|
-  #   if hook.name == "web" and hook.config.url == hook_url then
-  #     hooks_to_delete.push(hook)
-  #   end
-  # end
-  # hooks_to_delete.each do |hook|
-  #   github.repos.hooks.delete org, repo, hook.id
-  # end
-  # github.repos.hooks.create org, repo, name: "web", active: true, config:
-  #   {url: hook_url, content_type: "json"}
-
-  regex ||= /fix(es|ed)?|close(s|d)?/i
-
-  Fix = Struct.new(:message, :date, :files)
-  Spot = Struct.new(:file, :score)
-
-  repo_dir = File.join(settings['repo_dir'], "#{org}/#{repo}")
-  puts repo_dir
-  mkdir_p(repo_dir, mode: 0755)
-
-  grit_repo = Grit::Git.new repo_dir
-  process = grit_repo.clone({progress: true, process_info: true},
-    "https://#{login}:#{password}@github.com/#{org}/#{repo}", repo_dir)
-  print process[2]
-
-  branch = 'master'
-  fixes = []
-  grit_repo = Grit::Repo.new repo_dir
-  tree = grit_repo.tree(branch)
-  commit_list = grit_repo.git.rev_list({:max_count => false, :no_merges => true, :pretty => "raw", :timeout => false}, branch)
-  Grit::Commit.list_from_string(grit_repo, commit_list).each do |commit|
-    if commit.message =~ regex
-      files = commit.stats.files.map {|s| s.first}.select{ |s| tree/s }
-      fixes << Fix.new(commit.short_message, commit.date, files)
-    end
+  settings_repos.each do |repo|
+    repo = Repo.new(repo)
+    repos[repo.name] = repo
   end
 
-  hotspots = Hash.new(0)
-  fixes.each do |fix|
-    fix.files.each do |file|
-      t = 1 - ((Time.now - fix.date).to_f / (Time.now - fixes.last.date))
-      hotspots[file] += 1/(1+Math.exp((-12*t)+12))
-    end
+  repos.each do |name, repo|
+    #repo.set_hooks
+    repo.find_hotspots
   end
+end
 
-  spots = hotspots.sort_by {|k,v| v}.reverse.collect do |spot|
-    Spot.new(spot.first, sprintf('%.4f', spot.last))
-  end
 
-  if all_hotspots[org] == nil
-    all_hotspots[org] = {}
-  end
-  all_hotspots[org][repo] = spots
+post "/api/:org/:name" do
+  puts params
+  params
+end
 
-  post "/api/:org/:repo/:name" do
-    puts params
-    params
-  end
+get "/hotspots/:org/:name" do |org, name|
+  haml :hotspots, locals:{ :repo => repos[name] }
+end
 
-  get "/hotspots/:org/:repo" do
-    haml :hotspots, locals:{ :hotspots => all_hotspots[org][repo], :org => org, :repo => repo }
-  end
-
-  get '/' do
-    haml :index, locals:{ :all_hotspots => all_hotspots }
-  end
+get '/' do
+  haml :index, locals:{ :repos => repos }
 end
---
+++ repo.rb
@@ -0,0 +1,74 @@
+require 'ostruct'
+
+require 'debugger'
+require 'grit'
+
+include FileUtils
+
+class Repo < OpenStruct
+  @@Fix = Struct.new(:message, :date, :files)
+  @@Spot = Struct.new(:file, :score)
+
+  def initialize(repo)
+    super(repo)
+    self.password = CGI::escape self.password
+  end
+
+  def set_hooks()
+    hook_url = "#{$settings['address']}/api/#{self.org}/#{self.name}"
+
+    github = Github.new basic_auth: "#{self.login}:#{self.password}"
+    hooks = github.repos.hooks.all self.org, self.name
+    hooks_to_delete = []
+    hooks.each do |hook|
+      if hook.name == "web" and hook.config.url == hook_url then
+        hooks_to_delete.push(hook)
+      end
+    end
+    hooks_to_delete.each do |hook|
+      github.repos.hooks.delete self.org, self.name, hook.id
+    end
+    github.repos.hooks.create self.org, self.name, name: "web", active: true, config:
+      {url: hook_url, content_type: "json"}
+  end
+
+  def find_hotspots(branch='master')
+    regex = /fix(es|ed)?|close(s|d)?/i
+
+    repo_dir = File.join($settings['repo_dir'], "#{self.org}/#{self.name}")
+    puts repo_dir
+    mkdir_p(repo_dir, mode: 0755)
+
+    grit_repo = Grit::Git.new repo_dir
+    process = grit_repo.clone({progress: true, process_info: true},
+      "https://#{self.login}:#{self.password}@github.com/#{self.org}/#{self.name}", repo_dir)
+    print process[2]
+
+    fixes = []
+    grit_repo = Grit::Repo.new repo_dir
+    tree = grit_repo.tree(branch)
+    commit_list = grit_repo.git.rev_list({:max_count => false, :no_merges => true, :pretty => "raw", :timeout => false}, branch)
+    Grit::Commit.list_from_string(grit_repo, commit_list).each do |commit|
+      if commit.message =~ regex
+        # TODO: what does this line do?
+        files = commit.stats.files.map {|s| s.first}.select{ |s| tree/s }
+        fixes << @@Fix.new(commit.short_message, commit.date, files)
+      end
+    end
+
+    hotspots = Hash.new(0)
+    fixes.each do |fix|
+      fix.files.each do |file|
+        t = 1 - ((Time.now - fix.date).to_f / (Time.now - fixes.last.date))
+        hotspots[file] += 1/(1+Math.exp((-12*t)+12))
+      end
+    end
+
+    spots = hotspots.sort_by {|k,v| v}.reverse.collect do |spot|
+      @@Spot.new(spot.first, sprintf('%.4f', spot.last))
+    end
+
+    self.spots = spots
+  end
+  
+end
