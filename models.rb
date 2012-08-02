5f93d3bb06ec7e1a529f9db545c2d768615b83b0
Geoff Greer <geoff@greer.fm>
2012-08-02 16:20:24 -0700

    Create tables

--- models.rb
+++ models.rb
@@ -2,4 +2,22 @@ require 'sequel'
 
 $DB = Sequel.sqlite('db.sqlite')
 
+$DB.create_table? :projects do
+  primary_key :id
+  String :access_token
+  String :org, null: false
+  String :name, null: false
+  # TODO: remove me?
+  String :last_sha
+  unique [:org, :name]
+end
+
+$DB.create_table? :events do
+  foreign_key :project_id, :projects, null: false
+  DateTime :date, null: false
+  String :sha
+  String :file, null: false
+  unique [:sha, :file]
+end
+
 Dir[File.dirname(__FILE__) + '/models/*.rb'].each {|file| require file }
